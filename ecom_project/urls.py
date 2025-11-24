5eee147e7b6c6b70dd4d4e841a002970e13f50e7
Rohit Rajvaidya <rohit.rajvaidyaa@gmail.com>
2025-11-24 22:46:36 +0530

    code reformat with black

--- ecom_project/urls.py
+++ ecom_project/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
-    path("orders/", include("orders.urls")),
     path("accounts/", include("accounts.urls")),
 ]
 

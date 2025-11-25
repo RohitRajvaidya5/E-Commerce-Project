002d1647d06e987fa6f8369e4c1973005ad5ef62
Rohit Rajvaidya <rohit.rajvaidyaa@gmail.com>
2025-11-25 10:00:29 +0530

    storage update

--- ecom_project/settings.py
+++ ecom_project/settings.py
@@ -137,6 +137,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 #     BASE_DIR / "static",
 # ]
 
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
